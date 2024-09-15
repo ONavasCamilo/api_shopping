f6d09fe9e5c2b91e4b6b6daf6c236019c1d67890
Camilo Navas <navasochoa@hotmail.com>
2024-09-14 20:49:09 -0500

    delete confirm password ruta signup

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -4,7 +4,6 @@ import usersRouter from "./users.routes";
 import productsRouter from "./products.routes";
 import categoriesRouter from "./categories.routes";
 import ordersRouter from "./orders.routes";
-import filesRouter from "./files.routes";
 
 const router = Router();
 
@@ -13,6 +12,5 @@ router.use("/users", usersRouter);
 router.use("/products", productsRouter);
 router.use("/categories", categoriesRouter);
 router.use("/orders", ordersRouter);
-router.use("/files", filesRouter);
 
 export default router;

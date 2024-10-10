8c578cd9d0824413243cb34271b516ad7a2ca119
Camilo Navas <navasochoa@hotmail.com>
2024-10-10 17:33:46 -0500

    products filter by category name

--- src/seeders/category.seeder.ts
+++ src/seeders/category.seeder.ts
@@ -5,14 +5,12 @@ enum CategoryEnum {
   SUDADERAS = "sudaderas",
   CAMISETAS = "camisetas",
   ACCESORIOS = "accesorios",
-  HOODIES = "hoodies",
 }
 
 const INITIAL_CATEGORIES = [
   CategoryEnum.SUDADERAS,
   CategoryEnum.CAMISETAS,
   CategoryEnum.ACCESORIOS,
-  CategoryEnum.HOODIES,
 ];
 
 export const seedCategories = (): Promise<InsertResult>[] => {

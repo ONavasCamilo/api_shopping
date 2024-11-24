b9d136319a7c99a208ee961ed52686b15274cb4c
Camilo Navas <navasochoa@hotmail.com>
2024-11-24 13:48:58 -0500

    seeder groupProducts

--- src/seeders/groupProducts.seeder.ts
+++ src/seeders/groupProducts.seeder.ts
@@ -1,19 +1,38 @@
+import GroupProductModel from "../repositories/groupProduct.repository";
+import ProductModel from "../repositories/product.repository";
 import INITIAL_PRODUCTS from "../utils/data.json";
 
 export const seedGroupProducts = async () => {
-  // deberia retornar una promesa que agregue a la entidad groupProducts los diferentes grupos basandonos en los nombres que se repitieron por lo menos una vez y añadiendo al mismo grupo los productos con el mismo nombre
-  const groupProductsPromises = INITIAL_PRODUCTS.map((product) => {
-    const nameProducts: any = [];  // ["camiseta 1"]
-    if (nameProducts.length === 0) {
-        nameProducts.push(product.name)
-    } else {
-        for (let i = 0; i < nameProducts.length; i++) {
-            if (product.name === nameProducts[i]) {
-                
-            }
-        }
+
+  const groupsMap: { [name: string]: any[] } = {};
+
+  INITIAL_PRODUCTS.forEach(product => {
+    if (!groupsMap[product.name]) {
+      groupsMap[product.name] = [];
+    }
+    groupsMap[product.name].push(product);
+  });
+
+  const groupProductsPromises = Object.entries(groupsMap).map(async ([name, products]) => {
+    let group = await GroupProductModel.findOne({ where: { name } });
+    if (!group) {
+      group = GroupProductModel.create({ name });
+      await GroupProductModel.save(group);
     }
 
+    const productPromises = products.map(async productData => {
+      const product = ProductModel.create({
+        name: productData.name,
+        price: productData.price,
+        stock: productData.stock,
+        imgUrl: productData.imgUrl,
+        groupProducts: group
+      });
+      await ProductModel.save(product);
+    });
+
+    return Promise.all(productPromises);
   });
-  return groupProductsPromises;
+
+  await Promise.all(groupProductsPromises);
 };
--- src/seeders/index.seeder.ts
+++ src/seeders/index.seeder.ts
@@ -1,11 +1,15 @@
 import { seedCategories } from "./category.seeder";
+import { seedGroupProducts } from "./groupProducts.seeder";
 import { seedProducts } from "./products.seeder";
 import { seedRoles } from "./role.seeder";
 
 async function initSeeders() {
   console.log("Iniciando creación seeders..");
-  const initialSeeders = [...seedRoles(), ...seedCategories()];
-  await Promise.all(initialSeeders);
+  await Promise.all([
+    seedRoles(),
+    seedCategories(),
+    seedGroupProducts()
+  ]);
   const products = await seedProducts();
   await Promise.all(products);
   console.log("Finished roles seeders");

03c8dd78ca5dbed4246a942c56728808786b76c3
Camilo Navas <navasochoa@hotmail.com>
2024-11-26 19:37:48 -0500

    detele repository groupProducts

--- src/repositories/groupProduct.repository.ts
+++
@@ -1,6 +0,0 @@
-import { AppDataSource } from "../config/typeorm.config";
-import { GroupProducts } from "../entities/groupProduct.entity";
-
-const GroupProductModel = AppDataSource.getRepository(GroupProducts);
-
-export default GroupProductModel;
